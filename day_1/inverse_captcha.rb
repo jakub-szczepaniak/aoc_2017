cca8a24f902c7677dd085e3ce15e63c990a59f25
Jakub Szczepaniak <jakub.szczepaniak@milengo.com>
2017-12-05 20:00:43 +0100

    we now handle circular input

--- day_1/inverse_captcha.rb
+++ day_1/inverse_captcha.rb
@@ -1,4 +1,8 @@
 def inverse_captcha(input)
   fail ArgumentError if input.count == 0 || input.count == 1
-  0
+  captcha = 0
+  if input.first == input.last
+    captcha += input.first
+  end
+  captcha
 end
--- day_1/test_inverse_captcha.rb
+++ day_1/test_inverse_captcha.rb
@@ -7,10 +7,20 @@ class TestInverseCaptcha < MiniTest::Test
   def test_raises_argument_error_when_empty_input
     assert_raises(ArgumentError) { inverse_captcha [] }
   end
+
   def test_raises_argument_error_when_only_one_item_input
     assert_raises(ArgumentError) { inverse_captcha [1] }
   end
+
   def test_inverse_captcha_is_0_when_different_numbers
     assert_equal 0, inverse_captcha([1, 2])
+  end
+
+  def test_it_is_1_for_circular_input_are_repeated
+    assert_equal 1, inverse_captcha([1, 2, 1])
+  end
+
+  def test_it_is_2_for_circular_input_are_repeated
+    assert_equal 2, inverse_captcha([2, 1, 2])
   end
 end
