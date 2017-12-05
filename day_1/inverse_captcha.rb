fb7f25f78337d27afd5be488560ca5d4e42f15c9
Jakub Szczepaniak <jakub.szczepaniak@milengo.com>
2017-12-05 19:41:34 +0100

    we return 0 when items are not repeated

--- day_1/inverse_captcha.rb
+++ day_1/inverse_captcha.rb
@@ -1,3 +1,4 @@
 def inverse_captcha(input)
-  fail ArgumentError if input.count == 0
+  fail ArgumentError if input.count == 0 || input.count == 1
+  0
 end
--- day_1/test_inverse_captcha.rb
+++ day_1/test_inverse_captcha.rb
@@ -7,4 +7,10 @@ class TestInverseCaptcha < MiniTest::Test
   def test_raises_argument_error_when_empty_input
     assert_raises(ArgumentError) { inverse_captcha [] }
   end
+  def test_raises_argument_error_when_only_one_item_input
+    assert_raises(ArgumentError) { inverse_captcha [1] }
+  end
+  def test_inverse_captcha_is_0_when_different_numbers
+    assert_equal 0, inverse_captcha([1, 2])
+  end
 end
