88c483b399dfcfdb089e2e9d3a449cb52a0cc57d
Jakub Szczepaniak <jakub.szczepaniak@milengo.com>
2017-12-07 20:53:19 +0100

    we now calculate checksum for each of the lines

---
+++ day_2/corruption_captcha.rb
@@ -0,0 +1,3 @@
+def line_checksum(input)
+  input.max - input.min
+end
--- day_2/test_corruption_captcha.rb
+++ day_2/test_corruption_captcha.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 gem 'minitest', '>= 5.0.0'
 require 'minitest/autorun'
-
+require_relative 'corruption_captcha.rb'
 
 class TestCorruptionCaptchaWithInput < MiniTest::Test
   def test_for_real_input_part_1
@@ -11,4 +11,16 @@ class TestCorruptionCaptchaWithInput < MiniTest::Test
       line.split(/\t/).map(&:to_i)
     end
   end
+end
+
+class TestCalculationCorruptionChecksum < MiniTest::Test
+  def test_returns_0_for_min_equals_max
+    input = [1, 1]
+    assert_equal 0, line_checksum(input)    
+  end
+  def test_returns_non_zero_for_example_1
+    input = [5, 1, 9, 5]
+    assert_equal(8, line_checksum(input))
+  end
+
 end
