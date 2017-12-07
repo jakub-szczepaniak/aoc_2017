a5a4ad85686b803cfeb09418016848e45a4982da
Jakub Szczepaniak <jakub.szczepaniak@milengo.com>
2017-12-07 21:20:02 +0100

    solution to day_2 part 1

--- day_2/corruption_captcha.rb
+++ day_2/corruption_captcha.rb
@@ -2,7 +2,22 @@ def line_checksum(input)
   input.max - input.min
 end
 
-def spreadsheet_checksum(spreadsheet)
+def division_checksum(input)
+  checksum_pair = input.permutation(2).select do |pair|
+    pair[0] % pair[1] == 0
+  end
+  checksum_pair.first[0]/checksum_pair.first[1]
+end
+
+def spreadsheet_checksum_2(spreadsheet)
+  checksum = 0
+  spreadsheet.each do |row|
+    checksum += division_checksum(row)
+  end
+  checksum
+end
+
+def spreadsheet_checksum_1(spreadsheet)
   checksum = 0
   spreadsheet.each do |row|
     checksum += line_checksum(row)
--- day_2/test_corruption_captcha.rb
+++ day_2/test_corruption_captcha.rb
@@ -10,7 +10,15 @@ class TestCorruptionCaptchaWithInput < MiniTest::Test
     input = input_lines.map do |line|
       line.split(/\t/).map(&:to_i)
     end
-    assert_equal 48_357, spreadsheet_checksum(input)
+    assert_equal 48_357, spreadsheet_checksum_1(input)
+  end
+  def test_for_real_input_part_2
+    input_file = File.read('test_input.txt')
+    input_lines = input_file.split(/\n/)
+    input = input_lines.map do |line|
+      line.split(/\t/).map(&:to_i)
+    end
+    assert_equal 351, spreadsheet_checksum_2(input)
   end
 end
 
@@ -25,6 +33,19 @@ class TestCalculationCorruptionChecksum < MiniTest::Test
   end
   def test_spreadsheet_checksum
     input = [[5, 1, 9, 5], [7, 5, 3], [2, 4, 6, 8]]
-    assert_equal 18, spreadsheet_checksum(input)
+    assert_equal 18, spreadsheet_checksum_1(input)
+  end
+end
+
+class TestCalculationDivisionChecksum < MiniTest::Test
+  def test_evenly_divided_part_1
+    input= [5, 9, 2, 8]
+    assert_equal 4, division_checksum(input)
+  end
+  def test_evenly_divided_checksum
+    input = [[5, 9, 2, 8], [9, 4, 7, 3], [3, 8, 6, 5]]
+
+    assert_equal 9, spreadsheet_checksum_2(input)
+
   end
 end
